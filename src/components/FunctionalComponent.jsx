b7fa17006abe673ebab1f6f1a05cdfc60c9c9736
김정혁 <jack1150@naver.com>
2023-03-26 21:06:16 +0900

    commit

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,4 +1,4 @@
-const functionalComponent = () => {
-  return <h1>Functional Component 입니다.</h1>;
+const FunctionalComponent = () => {
+  return <h1>Functional Component</h1>;
 };
-export default functionalComponent;
+export default FunctionalComponent;
