69fb4e97f6f118d8c2858166dbd5f9dfdfa55299
김정혁 <jack1150@naver.com>
2023-03-29 09:51:50 +0900

    eslint 기본설정 완료

--- src/components/ChangeObj.jsx
+++ src/components/ChangeObj.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ChangeObjProps from "./ChangeObjProps";
+import React from 'react';
+import ChangeObjProps from './ChangeObjProps';
 
 export default function ChangeObj() {
   const pororoObjArr = [
     {
-      name: "뽀로로",
+      name: '뽀로로',
       age: 5,
-      nickName: "사고뭉치",
+      nickName: '사고뭉치',
     },
     {
-      name: "루피",
+      name: '루피',
       age: 4,
-      nickName: "공주님",
+      nickName: '공주님',
     },
     {
-      name: "크롱이",
+      name: '크롱이',
       age: 5,
-      nickName: "장난꾸러기",
+      nickName: '장난꾸러기',
     },
   ];
 
